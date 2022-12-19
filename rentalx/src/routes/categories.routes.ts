31519c30c3444ce1a48c0a0bd62a58de51b40a5e
= <=>
2022-12-19 15:14:20 -0300

    feat: add multer upload middleware and a post route for the upload.

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory/factory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories/factory";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (req, res) =>
   createCategoryController.handle(req, res)
@@ -12,5 +17,13 @@ categoriesRoutes.post("/", (req, res) =>
 categoriesRoutes.get("/", (req, res) =>
   listCategoriesController.handle(req, res)
 );
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  const { file } = req;
+
+  console.log(file);
+
+  return res.send();
+});
 
 export { categoriesRoutes };
