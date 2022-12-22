444279cbdaa05170dcd1ca322ef4439490f17e0c
= <=>
2022-12-22 15:50:40 -0300

    feat: deletes file before altering the user avatar.

--- rentalx/src/modules/cars/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ rentalx/src/modules/cars/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { deleteFile } from "../../../../utils/file";
 import { IUsersRepository } from "../../../accounts/repositories/IUsersRepository";
 
 interface IRequest {
@@ -16,6 +17,8 @@ export class UpdateUserAvatarUseCase {
 
   async execute({ userId, avatarFile }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(userId);
+
+    if (user.avatar) await deleteFile(`./tmp/avatar/${user.avatar}`);
 
     user.avatar = avatarFile;
 
