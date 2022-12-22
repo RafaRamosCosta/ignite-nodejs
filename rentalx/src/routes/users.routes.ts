63f8b853b0433c6a35fe62c064819ce11a98c5b4
= <=>
2022-12-22 15:44:08 -0300

    feat: adds a patch route to alter the user avatar.

--- rentalx/src/routes/users.routes.ts
+++ rentalx/src/routes/users.routes.ts
@@ -1,11 +1,26 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
+import { ensureAuthentication } from "../middlewares/ensureAuthentication";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/cars/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
+
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 
 const createUserController = new CreateUserController();
 
 usersRoutes.post("/", createUserController.handle);
+
+const updateUserAvatarController = new UpdateUserAvatarController();
+
+usersRoutes.patch(
+  "/avatar",
+  ensureAuthentication,
+  uploadAvatar.single("avatar"),
+  updateUserAvatarController.handle
+);
 
 export { usersRoutes };
