b72a5adaa294e58869d87138c89c94c5f372b57a
RafaRamosCosta <rm94281@fiap.com.br>
2022-12-28 19:09:12 -0300

    feat: adds listAvailableCars get route.

--- rentalx/src/shared/infra/http/routes/cars.routes.ts
+++ rentalx/src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
 import { ensureAuthentication } from "@shared/infra/http/middlewares/ensureAuthentication";
 
@@ -13,5 +14,8 @@ carsRoutes.post(
   ensureAdmin,
   createCarController.handle
 );
+
+const listAvailableCarsController = new ListAvailableCarsController();
+carsRoutes.get("/available", listAvailableCarsController.handle);
 
 export { carsRoutes };
