d24934a8d1550b1addccfb2df695587d470f5cb1
RafaRamosCosta <rm94281@fiap.com.br>
2022-12-28 16:40:13 -0300

    feat: implements findAvailable method using typeorm query builder.

--- rentalx/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ rentalx/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -40,4 +40,25 @@ export class CarsRepository implements ICarsRepository {
     const car = await this.repository.findOne({ license_plate });
     return car;
   }
+
+  async findAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string
+  ): Promise<Car[]> {
+    const carsQuery = this.repository
+      .createQueryBuilder("cars")
+      .where("available = :available", { available: true });
+
+    if (brand) carsQuery.andWhere("brand = :brand", { brand });
+
+    if (category_id)
+      carsQuery.andWhere("category_id = :category_id", { category_id });
+
+    if (name) carsQuery.andWhere("name = :name", { name });
+
+    const cars = await carsQuery.getMany();
+
+    return cars;
+  }
 }
