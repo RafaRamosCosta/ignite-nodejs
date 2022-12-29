d74810ae286b50f2fdcee95b988ad304441baabe
RafaRamosCosta <rm94281@fiap.com.br>
2022-12-29 17:57:17 -0300

    feat: adds carSpecification post route.

--- rentalx/src/shared/infra/http/routes/cars.routes.ts
+++ rentalx/src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
 import { ensureAuthentication } from "@shared/infra/http/middlewares/ensureAuthentication";
@@ -17,5 +18,13 @@ carsRoutes.post(
 
 const listAvailableCarsController = new ListAvailableCarsController();
 carsRoutes.get("/available", listAvailableCarsController.handle);
+
+const createCarSpecificationController = new CreateCarSpecificationController();
+carsRoutes.post(
+  "/specifications/:id",
+  ensureAuthentication,
+  ensureAdmin,
+  createCarSpecificationController.handle
+);
 
 export { carsRoutes };
