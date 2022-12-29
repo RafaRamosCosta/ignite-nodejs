154fc527ebd6b73e7b10be806ce4d7bc5b46d5ce
RafaRamosCosta <rm94281@fiap.com.br>
2022-12-29 15:39:02 -0300

    feat: implements findById method.

--- rentalx/src/modules/cars/repositories/in-memory/InMemoryCarsRepository.ts
+++ rentalx/src/modules/cars/repositories/in-memory/InMemoryCarsRepository.ts
@@ -52,4 +52,8 @@ export class InMemoryCarsRepository implements ICarsRepository {
       return null;
     });
   }
+
+  async findById(id: string): Promise<Car> {
+    return this.cars.find((car) => car.id === id);
+  }
 }
