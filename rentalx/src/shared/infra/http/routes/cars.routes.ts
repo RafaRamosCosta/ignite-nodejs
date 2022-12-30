b336b43e6f88e17a609abcb97e7c26b9c1b8f9c0
RafaRamosCosta <rm94281@fiap.com.br>
2022-12-30 12:10:59 -0300

    refactor: moves uploadCarImage useCase to useCases folder.

--- rentalx/src/shared/infra/http/routes/cars.routes.ts
+++ rentalx/src/shared/infra/http/routes/cars.routes.ts
@@ -3,7 +3,7 @@ import multer from "multer";
 
 import uploadConfig from "@config/upload";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
-import { UploadCarImagesController } from "@modules/cars/useCases/createCar/uploadCarImage/UploadCarImagesController";
+import { UploadCarImagesController } from "@modules/cars/useCases/uploadCarImage/UploadCarImagesController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
