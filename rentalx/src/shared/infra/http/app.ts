6d176823decc853db5dc03f011cbf3211b050d1e
RafaRamosCosta <rm94281@fiap.com.br>
2022-12-30 16:53:52 -0300

    refactor: exporting app.

---
+++ rentalx/src/shared/infra/http/app.ts
@@ -0,0 +1,32 @@
+import "express-async-errors";
+
+import express, { NextFunction, Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
+
+import { AppError } from "@shared/errors/AppError";
+import createConnection from "@shared/infra/typeorm";
+
+import "@shared/container";
+import swaggerFile from "../../../swagger.json";
+import { router } from "./routes";
+
+createConnection();
+
+const app = express();
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError)
+    return res.status(err.statusCode).json({ message: err.message });
+
+  return res.status(500).json({
+    status: "error",
+    message: `Internal Server Error - ${err.message}`,
+  });
+});
+
+export { app };
