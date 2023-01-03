9603cfc51d996378a7f532b2867a87841ba64cb4
RafaRamosCosta <rm94281@fiap.com.br>
2023-01-03 11:51:44 -0300

    feat: adds findById method.

--- rentalx/src/modules/rentals/repositories/IRentalsRepository.ts
+++ rentalx/src/modules/rentals/repositories/IRentalsRepository.ts
@@ -5,4 +5,5 @@ export interface IRentalsRepository {
   create(data: ICreateRentalDTO): Promise<Rental>;
   findOpenRentalByCarId(car_id: string): Promise<Rental>;
   findOpenRentalByUserId(user_id: string): Promise<Rental>;
+  findById(id: string): Promise<Rental>;
 }
