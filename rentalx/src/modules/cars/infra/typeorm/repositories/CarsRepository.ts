f54d83f4645bd55b5ed80b1125d79ef1728e8b62
RafaRamosCosta <rm94281@fiap.com.br>
2023-01-03 09:39:40 -0300

    feat: implements updateStatus method.

--- rentalx/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ rentalx/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -69,4 +69,14 @@ export class CarsRepository implements ICarsRepository {
   async findById(id: string): Promise<Car> {
     return this.repository.findOne(id);
   }
+
+  async updateStatus(id: string, available: boolean): Promise<void> {
+    await this.repository
+      .createQueryBuilder()
+      .update()
+      .set({ available })
+      .where("id = :id")
+      .setParameters({ id })
+      .execute();
+  }
 }
