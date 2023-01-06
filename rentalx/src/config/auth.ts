80ccc07448caabd566a349b879d4383a526a3b2b
RafaRamosCosta <rm94281@fiap.com.br>
2023-01-06 11:18:50 -0300

    chore: adds a authentication config file.

---
+++ rentalx/src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "9b8e28c955f001bd8bc1d810d114a00f",
+  token_expires_in: "15m",
+  secret_refresh_token: "608ae16db3e28e81272b5e21f5366326",
+  refresh_token_expires_in: "30d",
+  expires_refresh_token_days: 30,
+};
