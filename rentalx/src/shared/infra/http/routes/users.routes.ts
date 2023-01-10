f483abe6bc4a77f1b307dde49d4f09b2f4d83f41
RafaRamosCosta <rm94281@fiap.com.br>
2023-01-10 12:18:15 -0300

    chore: updates import.

--- rentalx/src/shared/infra/http/routes/users.routes.ts
+++ rentalx/src/shared/infra/http/routes/users.routes.ts
@@ -3,7 +3,7 @@ import multer from "multer";
 
 import uploadConfig from "@config/upload";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
-import { UpdateUserAvatarController } from "@modules/cars/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 import { ensureAuthentication } from "@shared/infra/http/middlewares/ensureAuthentication";
 
 const usersRoutes = Router();
