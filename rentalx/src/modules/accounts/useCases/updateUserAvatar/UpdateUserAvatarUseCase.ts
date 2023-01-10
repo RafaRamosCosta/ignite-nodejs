e365ec19b14e5237537f1d73ee1e273b961690c3
RafaRamosCosta <rm94281@fiap.com.br>
2023-01-10 14:55:30 -0300

    refactor: usesthe storageProvider delete & save methods

--- rentalx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ rentalx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
-import { deleteFile } from "@utils/file";
+import { IStorageProvider } from "@shared/container/providers/storageProvider/IStorageProvider";
 
 interface IRequest {
   userId: string;
@@ -12,13 +12,16 @@ interface IRequest {
 export class UpdateUserAvatarUseCase {
   constructor(
     @inject("UsersRepository")
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    @inject("StorageProvider") private storageProvider: IStorageProvider
   ) {}
 
   async execute({ userId, avatarFile }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(userId);
 
-    if (user.avatar) await deleteFile(`./tmp/avatar/${user.avatar}`);
+    if (user.avatar) await this.storageProvider.delete(user.avatar, "avatar");
+
+    await this.storageProvider.save(avatarFile, "avatar");
 
     user.avatar = avatarFile;
 
