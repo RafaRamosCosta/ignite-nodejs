8cdd74634b0e06e819722e1641d6d9163f3bfe5e
RafaRamosCosta <rm94281@fiap.com.br>
2023-01-11 11:15:00 -0300

    feat: adds avatar_url method.

--- rentalx/src/modules/accounts/infra/typeorm/entities/User.ts
+++ rentalx/src/modules/accounts/infra/typeorm/entities/User.ts
@@ -1,3 +1,4 @@
+import { Expose } from "class-transformer";
 import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
@@ -29,5 +30,19 @@ export class User {
 
   constructor() {
     if (!this.id) this.id = uuidv4();
+  }
+
+  @Expose({ name: "avatar_url" })
+  avatar_url(): string {
+    switch (process.env.disk) {
+      case "local":
+        return `${process.env.APP_API_URL}/avatar/${this.avatar}`;
+
+      case "s3":
+        return `${process.env.AWS_BUCKET_URL}/avatar/${this.avatar}`;
+
+      default:
+        return null;
+    }
   }
 }
